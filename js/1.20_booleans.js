4ccce036a4b7ae4fa4dc0afc2ea5909dac92be2e
Matthew Kory <matthewskory@gmail.com>
2016-07-14 10:18:37 -0400

    boolean file added

---
+++ js/1.20_booleans.js
@@ -0,0 +1,21 @@
+//boolean logic allows for only 2 possible answers true or false (1 or 0, on or off)
+
+var x = true //true
+	x ==1 //true
+	x == 0 //false
+
+
+var y = false // false
+ 	y == 1 //false
+	y == 0 //true
+
+var numOne = 1 
+var numTwo = 2 
+
+numOne > numTwo //false
+numOne < numTwo //true
+
+//undefined, null, "", 0,-0, NaN and False  all are considered False
+//objects, strings with values and numbers not 0 or -0 are all True
+
+
