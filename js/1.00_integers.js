a115ced45b0554f7089461f3ce134238382120f5
Matthew Kory <matthewskory@gmail.com>
2016-07-14 09:43:39 -0400

    created integers.js file

---
+++ js/1.00_integers.js
@@ -0,0 +1,17 @@
+//only one type of number in javascript
+
+var x = 3;
+var y = 3.33;
+
+//exponents can be written as follows
+
+var z = 554e4; // 5540000
+var w = 34e-2; // 0.34
+
+//add, subtract, multiply, divide and modulus
+
+var a = 43 + 4; // 47
+var b = 10 / 5; //2
+var c = 10 % 2; // 0 no remanders are left after the operation 0 even 1 odd
+
+console.log(b);
